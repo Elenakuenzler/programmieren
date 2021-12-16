cc6d976b3cccf9bc6b9ecc04e0d7738042eb9836
Elenakuenzler <96241979+Elenakuenzler@users.noreply.github.com>
2021-12-16 14:22:26 +0100

    Add files via upload

---
+++ Login_mit_Benutzername_und_Passwort.py
@@ -0,0 +1,7 @@
+Benutzername=str(input("Benutzername eingeben: "))
+Passwort=str(input("Passwort eineben: "))
+
+if Benutzername==("Apfelkönig123")and Passwort==("Apfelstrudel8"):
+    print("Herzlich Willkommen") 
+else:
+    print("Benutzer nicht gefunden")
